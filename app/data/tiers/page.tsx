c9fc8013361ad890b8817da7f37800a7c941a8f9
oscar Info <racso1607@gmail.com>
2025-02-22 20:02:52 -0500

    add supabase client and page to render data

---
+++ app/data/tiers/page.tsx
@@ -0,0 +1,13 @@
+import { supabase } from "@/utils/supabase/supabaseClient";
+
+export default async function Tiers() {
+  const { data: tiers, error } = await supabase.from('tiers').select('*');
+
+  if (error) {
+    console.error("Error fetching tiers:", error);
+    return <div>Error fetching tiers</div>;
+  }
+
+  console.log("tiers", tiers);
+  return <pre>{JSON.stringify(tiers, null, 2)}</pre>;
+}
