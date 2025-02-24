83baf3c9b66ae49f441f86a1a750470189bba50e
oscar Info <racso1607@gmail.com>
2025-02-24 05:02:25 -0500

    fixes zoom controls

--- app/pages/orgChart.tsx
+++ app/pages/orgChart.tsx
@@ -6,11 +6,16 @@ import EmployeeList from "../components/organigram/EmployeeList";
 import { ZoomControls } from "../components/organigram/ZoomControls";
 import { useOrgChart } from "../hooks/useOrgChart";
 import { Employee } from "../types/types";
+import {
+  TransformComponent,
+  TransformWrapper,
+} from "react-zoom-pan-pinch";
 
 export default function OrgChart() {
-  const [zoom, setZoom] = useState(100);
   const [employeeSheetOpen, setEmployeeSheetOpen] = useState(false);
   const [selectedEmployees, setSelectedEmployees] = useState<Employee[]>([]);
+  const [transformWrapperDisabled, setTransformWrapperDisabled] = useState(false);
+
   const {
     tiers,
     activeId,
@@ -30,22 +35,37 @@ export default function OrgChart() {
 
   return (
     <div className="min-h-screen bg-background text-foreground p-6">
-      <OrgChartContent
-        tiers={tiers}
-        activeId={activeId ? activeId.toString() : null}
-        zoom={zoom}
-        onDragStart={handleDragStart}
-        onDragEnd={handleDragEnd}
-        onDelete={handleDelete}
-        onEmployeeSheetOpen={handleEmployeeSheetOpen}
-        findPosition={findPosition}
-      />
-      <ZoomControls zoom={zoom} setZoom={setZoom} />
-      <EmployeeList
-        open={employeeSheetOpen}
-        onOpenChange={setEmployeeSheetOpen}
-        employees={selectedEmployees}
-      />
+      <TransformWrapper
+        initialScale={1}
+        minScale={0.5}
+        disabled={transformWrapperDisabled}
+      >
+        <TransformComponent
+          wrapperStyle={{ width: "100%", height: "100%", paddingTop: "12px" }}
+          contentStyle={{ width: "100%", height: "100%" }}
+        >
+          <OrgChartContent
+            tiers={tiers}
+            activeId={activeId ? activeId.toString() : null}
+            onDragStart={(event) =>
+              handleDragStart(event, setTransformWrapperDisabled)
+            }
+            onDragEnd={(event) =>
+              handleDragEnd(event, setTransformWrapperDisabled)
+            }
+            onDelete={handleDelete}
+            onEmployeeSheetOpen={handleEmployeeSheetOpen}
+            findPosition={findPosition}
+          />
+        </TransformComponent>
+
+        <ZoomControls />
+        <EmployeeList
+          open={employeeSheetOpen}
+          onOpenChange={setEmployeeSheetOpen}
+          employees={selectedEmployees}
+        />
+      </TransformWrapper>
     </div>
   );
 }
